526ea1cf27704fbba58447ed92dc7330f0714aff
Thomas Pelletier <thomaspelletier44@gmail.com>
2023-02-07 17:24:22 +0100

    update infra

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: { disableHostCheck: true }
+  devServer: { disableHostCheck: true, }
 })
