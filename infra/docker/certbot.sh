21961250447056c1ed70fb7faf25680ed339a475
Thomas Pelletier <thomaspelletier44@gmail.com>
2023-02-07 15:09:50 +0100

    update infra

--- infra/docker/certbot.sh
+++ infra/docker/certbot.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source ../.secret
 docker run --rm -it -v /etc/letsencrypt:/etc/letsencrypt -v $(pwd):/tmp certbot/dns-ovh:v2.2.0 certonly --dns-ovh --dns-ovh-credentials /tmp/.ovhapi --non-interactive --agree-tos --email thomaspelletier44@gmail.com -d *.adminadmin.fr
 docker exec nginx nginx -s reload
