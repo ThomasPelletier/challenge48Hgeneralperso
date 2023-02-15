8607a817d850f5abdd90a0acf3f0ce30d07954a5
Thomas Pelletier <thomaspelletier44@gmail.com>
2023-02-15 08:58:03 +0100

    fix docker rights first execution

--- mysql/setupHub.sql
+++ mysql/setupHub.sql
@@ -2,7 +2,7 @@ create database if not exists hub;
 use hub;
 create table if not exists Instance(
   id int auto_increment not null,
-  ip varchar(20) not null,
+  ip varchar(255) not null,
   domaine varchar(255) not null,
   primary key(id)
 );
